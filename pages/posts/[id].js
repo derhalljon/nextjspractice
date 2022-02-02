11c8cbc0a7b8aa96451e53d5d99d9016067ac23f
derhalljon <halldorjons20@gmail.com>
2022-02-02 23:50:50 +0000

    lesson 2 with dates and stuff

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,9 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
-import Date from '../..components/date'
+import Date from '../../components/date'
+import utilStyles from '../../styles/utils.module.css'
+
 
 
 export default function Post({ postData }) {
@@ -10,12 +12,14 @@ export default function Post({ postData }) {
             <Head>
             <title>{postData.title}</title>
             </Head>
-            <br />
-            {postData.id}
-            <br />
-            <Date dateString={postData.date} />
-            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-        </Layout>
+            <article>
+                <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+                <div className={utilStyles.lightText}>
+                    <Date dateString={postData.date} />
+                </div>
+                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+            </article>
+        </Layout>        
     )
 }
 
